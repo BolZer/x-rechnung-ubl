afb1caec3d2bebb5bbe92004b0103abbdea7b16f
Jan Nöhles <jan.noehles@easybill.de>
2024-03-10 18:35:06 +0100

    Add models

--- src/XRechnung3/Models/CreditNoteLine.php
+++ src/XRechnung3/Models/CreditNoteLine.php
@@ -7,6 +7,7 @@ namespace Bolzer\XRechnungUbl\XRechnung3\Models;
 use JMS\Serializer\Annotation\SerializedName;
 use JMS\Serializer\Annotation\Type;
 use JMS\Serializer\Annotation\XmlElement;
+use JMS\Serializer\Annotation\XmlList;
 
 final class CreditNoteLine
 {
@@ -39,4 +40,9 @@ final class CreditNoteLine
     #[XmlElement(cdata: false, namespace: 'urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2')]
     #[SerializedName('OrderLineReference')]
     private ?OrderLineReference $orderLineReference = null;
+
+    #[Type('array<Bolzer\XRechnungUbl\XRechnung3\Models\Item>')]
+    #[SerializedName('Item')]
+    #[XmlList(entry: 'Item', inline: true, namespace: 'urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2')]
+    private ?array $item = null;
 }
---
+++ src/XRechnung3/Models/Item.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bolzer\XRechnungUbl\XRechnung3\Models;
+
+use JMS\Serializer\Annotation\SerializedName;
+use JMS\Serializer\Annotation\Type;
+use JMS\Serializer\Annotation\XmlElement;
+
+final class Item
+{
+    #[Type('string')]
+    #[XmlElement(cdata: false, namespace: 'urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2')]
+    #[SerializedName('Description')]
+    private ?string $description = null;
+
+    #[Type('string')]
+    #[XmlElement(cdata: false, namespace: 'urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2')]
+    #[SerializedName('Name')]
+    private ?string $name = null;
+
+    #[Type(SellersItemIdentification::class)]
+    #[XmlElement(cdata: false, namespace: 'urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2')]
+    #[SerializedName('SellersItemIdentification')]
+    private ?SellersItemIdentification $sellersItemIdentification = null;
+
+    #[Type(TaxCategory::class)]
+    #[XmlElement(cdata: false, namespace: 'urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2')]
+    #[SerializedName('ClassifiedTaxCategory')]
+    private ?TaxCategory $classifiedTaxCategory = null;
+}
